5d2a36348c49f9ce97613ba192d6e8287b8d7370
vidhyasree-m <mvidhyasree12177@gmail.com>
2025-02-05 14:11:54 +0530

    commit Day4

---
+++ wipro_java_project/src/com/wipro/java/collection/Array.java
@@ -0,0 +1,21 @@
+package com.wipro.java.collection;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//declaration and allocating memory size of an array
+		int[] arr = new int[5];
+		//initializing the values
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		arr[3] = 40;
+		arr[4] = 50;
+		
+		
+		
+
+	}
+
+}
