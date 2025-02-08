e70ef0a022d343a50d7f814f015611a78c7912c2
vidhyasree-m <mvidhyasree12177@gmail.com>
2025-02-08 13:08:22 +0530

    commit Day7

--- wipro_java_project/src/com/wipro/java/java8/usecase/Employee.java
+++ wipro_java_project/src/com/wipro/java/java8/usecase/Employee.java
@@ -8,7 +8,7 @@ public class Employee {
 	
 	//private variables 
 	   private int id; 
-	    private String name; 
+	   private String name; 
 	    private String department; 
 	    private double salary; 
 	    private LocalDate joiningDate; 
@@ -24,16 +24,58 @@ public class Employee {
 	        this.joiningDate = joiningDate; 
 	     } 
 	 
-	    // Getters and Setters 
-	    public int getId() { return id; } 
-	    public String getName() { return name; } 
-	    public String getDepartment() { return department; } 
-	    public double getSalary() { return salary; }
-	    public LocalDate getJoiningDate() { return joiningDate; } 
-	    public void setName(String name) { this.name = name; } 
-	    public void setDepartment(String department) { this.department = department; } 
-	    public void setSalary(double salary) { this.salary = salary; } 
-	 
+	 //getters and setters
+    public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getDepartment() {
+		return department;
+	}
+
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+
+	public double getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+
+	public LocalDate getJoiningDate() {
+		return joiningDate;
+	}
+
+
+	public void setJoiningDate(LocalDate joiningDate) {
+		this.joiningDate = joiningDate;
+	}
+
+
+		
 	    @Override 
 	    public String toString() { 
 	        return "Employee { " + "ID=" + id + ", Name='" + name + '\'' +  
